d696fcbf44d37ef6c18c721cb35ed7c52d9c76d9
dimionikk <dimionik952@gmail.com>
2024-11-10 12:02:13 +0200

    Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,7 +1,8 @@
-﻿#include <stdio.h>  // бібліотека вводу виводу
-int main() { // початок програми
-	char username[16]; // змінна яка містить масив
-	printf("you name:"); 
-	scanf("%s", username, sizeof(username)); // %s текстовий рядок 
-	printf("helloo, %s\n", username);
+#include <stdio.h> // бібліотека вводу\виводу
+int main() {
+		char username[16]; // змінна яка містить масив
+		printf("you name:");
+		scanf_s("%s", username, sizeof(username)); // %s текстовий рядок 
+		printf("hello, %s\n", username);
+	
 }
