17bd5f49319ce5fbc89bc4ffb209e6a201d6d382
dimionikk <dimionik952@gmail.com>
2024-11-10 11:51:20 +0200

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>  // бібліотека вводу виводу
+int main() { // початок програми
+	char username[16]; // змінна яка містить масив
+	printf("you name:"); 
+	scanf("%s", username, sizeof(username)); // %s текстовий рядок 
+	printf("helloo, %s\n", username);
+}
