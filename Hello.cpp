7ea608d9c05314d58b5b2e96ab17e576dc145adf
dimionikk <dimionik952@gmail.com>
2024-11-10 12:03:09 +0200

    Delete Hello.cpp

--- Hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> // бібліотека вводу\виводу
-int main() {
-		char username[16]; // змінна яка містить масив
-		printf("you name:");
-		scanf_s("%s", username, sizeof(username)); // %s текстовий рядок 
-		printf("hello, %s\n", username);
-	
-}
